ee54c592951b81a8f9f665e0de203b58bbe42c65
nickw <nickw4426@gmail.com>
2020-10-07 14:24:03 +0000

    added pop with error check for empty stack

--- main.py
+++ main.py
@@ -10,20 +10,21 @@ class Stack:
 
     # Add an 'if' statement here, to stop the item being removed
     # if the stack is empty.
+    if len(self.internalArray) == 0:
+      print("Stack is empty - cannot pop")
+    else:
+      # Store the current top of the stack in a variable
+      a = self.internalArray[-1]
 
-    # Store the current top of the stack in a variable
-    a = self.internalArray[-1]
+      # THEN, we remove the top item
+      # This is now a safe operation because we've 
+      # stored the top item in a variable so we don't
+      # lose it!
+      del self.internalArray[-1]
 
-    # THEN, we remove the top item
-    # This is now a safe operation because we've 
-    # stored the top item in a variable so we don't
-    # lose it!
-    del self.internalArray[-1]
-
-    # THEN, we return the variable which holds the
-    # previous top item
-    return a
-
+      # THEN, we return the variable which holds the
+      # previous top item
+      return a
 
 # 'Main program'
 # Outside the class (because these statements are not indented)
@@ -40,5 +41,6 @@ c = stack1.pop()
 print(c)
 
 d = stack1.pop()
-print(d)
+if not d is None:
+  print(d)
 
